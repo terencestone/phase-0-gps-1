a70bd102491171978954cfab1eee72835b4cb396
Terence Stone <terence.stone1@gmail.com>
2016-09-05 16:51:21 -0400

    Complete pad exercise

--- pad.js
+++ pad.js
@@ -1,3 +1,13 @@
 Array.prototype.pad = function(minSize, val) {
+  new_arr = this
+  if (val === undefined) { val = null; };
+  if (minSize <= this.length) {
+    return this;
+  } else {
+    for (i = new_arr.length; i < minSize; i++) {
+      new_arr.push(val);
+    }
+    return new_arr;
+  }
 
 };
