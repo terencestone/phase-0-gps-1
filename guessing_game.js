473c100faf5a663b8ad4ce8e90d71a8df8091cf4
Terence Stone <terence.stone1@gmail.com>
2016-09-05 17:25:22 -0400

    Complete guessing game

--- guessing_game.js
+++ guessing_game.js
@@ -1,3 +1,25 @@
 function GuessingGame(answer) {
-  // Your code here!
+  this.answer = answer;
+  this.solved = false
+}
+
+GuessingGame.prototype.guess = function(guess) {
+  if (guess > this.answer) {
+    this.solved = false
+    return "high";
+  } else if (guess < this.answer) {
+    this.solved = false
+    return "low";
+  } else {
+    this.solved = true
+    return "correct"
+  }
+}
+
+GuessingGame.prototype.isSolved = function() {
+  if (this.solved) { 
+    return true; 
+  } else {
+    return false;
+  }
 }
