d953211464c15d29a06e0a855aabece87eb344f9
Terence Stone <terence.stone1@gmail.com>
2016-09-05 17:06:58 -0400

    Complete longest string exercise

--- longest_string.js
+++ longest_string.js
@@ -1,3 +1,13 @@
 function longestString(array) {
-  // Your code here!
+  if (array.length === 0) {
+    result = null
+  } else {
+    result = ""
+    for (i = 0; i < array.length; i++) { 
+      if (result.length < array[i].length) { 
+        result = array[i]; 
+      } 
+    }
+  }
+  return result;
 }
