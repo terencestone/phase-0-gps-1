b2f6da872c35b8bd5b8f5ccf0cc3dfcf7e6914c6
Terence Stone <terence.stone1@gmail.com>
2016-09-05 16:26:13 -0400

    Complete js average exercises

--- average.js
+++ average.js
@@ -1,17 +1,42 @@
 Array.prototype.total = function() {
-  // Your code here!
+  var sum = this.reduce(function(a, b) {return a + b}, 0);
+  return sum;
 };
 
 Array.prototype.mean = function() {
-  // Your code here!
+  return this.total() / this.length;
 };
 
 Array.prototype.median = function() {
-  // Your code here!
+  var halfIndex = Math.floor(this.length/2);
+  if (this.length % 2 ) {
+    return this[halfIndex]; 
+  } else {
+    return (this[halfIndex - 1] + this[halfIndex]) / 2;
+  }
 };
 
 Array.prototype.mode = function() {
-  // Your code here!
-  // Ex. Mode array:
-  // var mode = { 5:3, 6:3 }
+  var possibleModes = {}
+  var count = 0
+
+  this.forEach(function(num) {
+    if (possibleModes[num]) {
+      possibleModes[num]++;
+    } else {
+      possibleModes[num] = 1;
+    }
+  });
+
+  total = 0
+  Object.keys(possibleModes).forEach(function(key) {
+    n = possibleModes[key];
+    if (total < n) { total = 0; total += n }; 
+  });
+
+  Object.keys(possibleModes).forEach(function(key) {
+    if (possibleModes[key] !== total) {delete possibleModes[key]};
+  });
+
+  return possibleModes;
 };
--- triangle.js
+++ triangle.js
@@ -1,8 +1,7 @@
 function validTriangle(a, b, c) {
-  if (a === 0 || b === 0 || c === 0) {
+  if (a === 0 || b === 0 || c === 0 || (a === b && b !== c) || (b === c && c !== a) || (c === a && a !== b)) {
     return false;
   } else {
     return true;
   }
-
-}
+};
